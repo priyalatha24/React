44e1c190742369f9d4b791d1b1ce559ba5ba4351
Priyalatha <priyalatha24042002@gmail.com>
2024-01-09 09:31:42 +0530

    latlongfinding

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import './pages/script';
+import submit from './pages/script';
 
 function App() {
 
-  const submit = () => {
-    const latitude = document.getElementById('lat').value;
-    const longitude = document.getElementById('long').value;
-
-    fetch(`http://api.geonames.org/countrySubdivisionJSON?lat=${latitude}&lng=${longitude}&username=ranjith`)
-      .then(response => response.json())
-      .then(result => {
-        const innerHtml = `<p>country: ${result.adminName1}</p><p>countryName : ${result.countryName}</p>`;
-        
-        const resElement = document.getElementById("res");
-        resElement.innerHTML = innerHtml;
-        resElement.style.display = "block";
-        console.log(result);
-      })
-      .catch(error => console.log('error', error));
-  };
   return (
     <div class="container">
     <h1>GEO FORM</h1>
     <div class="item">
         <input type="text" placeholder="---Enter latitude---" id="lat"/>
         <input type="text" placeholder="---Enter longitude---" id="long"/>
-        <button type="submit" class="login-btn" onclick={submit}>SUBMIT</button>
+        <button onClick={submit}>SUBMIT</button>
         <div id="res" style={{display:'none'}}>
              
         </div>
